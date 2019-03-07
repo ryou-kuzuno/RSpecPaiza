5d796a02e548257d540f1568ec2b0ad1650ee94e
葛野遼 <ryou0933@gmail.com>
2019-03-07 16:27:49 +0900

    数字のみではオーケー

--- lib/paiza_B017.rb
+++ lib/paiza_B017.rb
@@ -1,6 +1,29 @@
 class Paiza_B017
 
+    # def initialize
+      
+    # end
+
     def hand_service(card:)
-      'NoPair'
+      words = card.split('')
+      answer = [0, 0, 0, 0]
+      words.each_with_index do |w, i|
+        t = 0
+        words.each do |w2|
+          t += 1 if w == w2 || w == '*' || w2 == '*'
+        end
+        answer[i] = t
+      end
+      if answer.count(2) == 4
+        'TwoPair'
+      elsif answer.count(2) == 2
+        'OnePair'
+      elsif answer.count(3) == 3
+        'ThreeCard'
+      elsif answer.count(4) == 4
+        'FourCard'
+      else
+        'NoPair'
+      end
     end
 end
--- spec/paiza_B017_spec.rb
+++ spec/paiza_B017_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 RSpec.describe Paiza_B017 do
   it '手役の強さ' do
-    result = Paiza_B017.new.hand_service(card: 'ABCD')
-    expect(result).to eq 'NoPair'
+    result = Paiza_B017.new.hand_service(card: 'ABAB')
+    expect(result).to eq 'TwoPair'
+  end
+
+  it '手役の強さ' do
+    result = Paiza_B017.new.hand_service(card: '*ZZD')
+    expect(result).to eq 'ThreeCard'
   end
 end
