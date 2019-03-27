f618cec4f36cb8acd5521c0cd8e1a650265cd7f8
葛野遼 <ryou0933@gmail.com>
2019-03-27 15:53:56 +0900

    隔離された町のゲートテストコード

--- lib/Paiza_隔離された町のゲート.rb
+++ lib/Paiza_隔離された町のゲート.rb
@@ -1,4 +1,24 @@
 class Isolated_city_gate
-    def coordinate(map_longitudinal:, map_]ateral:, character_movement:)
+    def coordinate(map_longitudinal:, map_lateral:, character_movement:)
+        x_coordinate = 0
+        y_coordinate = 0
+        valid_true = 0
+        character_movement.split('').each do |ope|
+            if ope == "R"
+              x_coordinate += 1
+            elsif ope == "L"
+              x_coordinate -= 1
+            elsif ope == "U"
+              y_coordinate += 1
+            elsif ope == "D"
+              y_coordinate -= 1
+            end
+            valid_true = 1 if x_coordinate < 0 || x_coordinate >= map_lateral || y_coordinate < 0 || y_coordinate >= map_longitudinal
+        end
+        if valid_true == 1
+          'invalid'
+        else
+          'valid'
+        end
     end
 end
--- spec/paiza_隔離された町のゲート_spec.rb
+++ spec/paiza_隔離された町のゲート_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 RSpec.describe Isolated_city_gate do
   describe 'キャラクターがどの座標にいるのか' do
     it '正しい入力' do
-      result = Isolated_city_gate.new.coordinate(map_longitudinal: 3, map_]ateral: 3, character_movement: 'URDRL')
+      result = Isolated_city_gate.new.coordinate(map_longitudinal: 3, map_lateral: 3, character_movement: 'URDRL')
       expect(result).to eq 'valid'
     end
 
     it '不正な入力' do
-      result = Isolated_city_gate.new.coordinate(map_longitudinal: 4, map_]ateral: 4, character_movement: 'UURRRRD')
+      result = Isolated_city_gate.new.coordinate(map_longitudinal: 4, map_lateral: 4, character_movement: 'UURRRRD')
       expect(result).to eq 'invalid'
     end
   end
